795ec696a59c11a75a00214e5c6a6102ed771644
BackEndCoder <steve.coverdale@gmail.com>
2014-01-15 20:04:52 +0000

    added Cake Core as submodule

--- Console/cake.php
+++ Console/cake.php
@@ -22,8 +22,8 @@ $ds = DIRECTORY_SEPARATOR;
 $dispatcher = 'Cake' . $ds . 'Console' . $ds . 'ShellDispatcher.php';
 
 if (function_exists('ini_set')) {
-	$root = dirname(dirname(dirname(__FILE__)));
-
+	//$root = dirname(dirname(dirname(__FILE__)));
+	$root = dirname(dirname(__FILE__)) . $ds . 'Core';
 	// the following line differs from its sibling
 	// /lib/Cake/Console/Templates/skel/Console/cake.php
 	ini_set('include_path', $root . $ds . 'lib' . PATH_SEPARATOR . ini_get('include_path'));

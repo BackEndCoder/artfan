e3d7a8179994fa5f056b295b33d342c2eaaca713
BackEndCoder <steve.coverdale@gmail.com>
2014-02-20 19:46:11 +0000

    Changed Routes for static giftcard to products

--- Config/routes.php
+++ Config/routes.php
@@ -25,6 +25,7 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
         Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+        Router::connect('/giftcards', array('controller' => 'products', 'action' => 'giftcard_index'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */

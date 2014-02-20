1528eb0f0eb33591a054b41c1e2c29f127484e85
BackEndCoder <steve.coverdale@gmail.com>
2014-02-20 20:19:39 +0000

    Removed HeaderController

--- Controller/ProductsController.php
+++ Controller/ProductsController.php
@@ -255,4 +255,32 @@ class ProductsController extends AppController {
     function getProducts() {
         return $this->Product->getProducts();
     }
+
+    public function getTotalCartPrice($id = null) {                     
+        $productsCtrl = new ProductsController();
+        $productsCtrl->constructClasses();      
+        
+        $productArray = $this->Session->read('cart');
+        $products = array();
+        if (count($productArray) > 0) {
+            foreach ($productArray as $cartItemKey => $cartItemValue) {
+                $product = $productsCtrl->Product->find('first', array('conditions' => array('Product.id' => $cartItemKey)));
+                if ($product != null) {
+                    $product['Product']['Quantity'] = $cartItemValue;
+                    $products[] = $product;
+                }
+            }
+        }
+        $cart_cnt = 0;
+        $total = 0;
+        foreach ($products as $product) :                                                       
+            $prod_charge    = $product['Product']['price'];
+            $prod_qty       = $product['Product']['Quantity'];
+            $total          += $prod_charge*$prod_qty; 
+            $cart_cnt++;
+        endforeach;             
+        return $total;
+    }   
+
+    
 }

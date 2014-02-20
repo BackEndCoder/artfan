ecc5e68f79238088703cf35274917c6d850372c9
BackEndCoder <steve.coverdale@gmail.com>
2014-02-20 22:40:18 +0000

    Updated Categories Controller with PHPCS

--- Controller/CategoriesController.php
+++ Controller/CategoriesController.php
@@ -1,24 +1,12 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of ProductsController
- *
- * @author Wilson<mailwilson007@gmail.com>
- */
 App::uses('AppController', 'Controller');
-
 class CategoriesController extends AppController {
 
-    public $name = 'Categories';
-    public $scaffold = '';
-    public $paginate = array('Category' => array('limit' => 10));
+	public $name = 'Categories';
 
-    public function getCategories() {
-        return $this->Category->getCategories();
-    }
+	public $paginate = array('Category' => array('limit' => 10));
+
+	public function getCategories() {
+		return $this->Category->getCategories();
+	}
 }

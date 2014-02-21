b0fc6fdc2b53c606637bc6682640264732c80749
BackEndCoder <steve.coverdale@gmail.com>
2014-02-21 23:27:34 +0000

    Contents Improvments

--- Controller/ContentsController.php
+++ Controller/ContentsController.php
@@ -15,55 +15,55 @@ class ContentsController extends AppController {
 
     public function add() {
         if ($this->request->is('post')) {
-            if ($this->Page->save($this->data)) {
+            if ($this->Content->save($this->data)) {
                 $this->redirect(array('action' => 'index'));
-                $this->Session->setFlash("The page has been saved");
+                $this->Session->setFlash("The content has been saved");
             } else {
-                $this->Session->setFlash('The page could not be saved. Please, try again.');
+                $this->Session->setFlash('The content could not be saved. Please, try again.');
             }
         }
     }
 
     public function edit($id = null) {
-        $this->Page->id = $id;
+        $this->Content->id = $id;
         if ($this->request->is('post') || $this->request->is('put')) {
-            if ($this->Page->save($this->request->data)) {
+            if ($this->Content->save($this->request->data)) {
                 $this->Session->setFlash('The product has been saved');
                 $this->redirect(array('action' => 'index'));
             } else {
                 $this->Session->setFlash('The product could not be saved. Please, try again.');
             }
         } else {
-            $this->request->data = $this->Page->read();
+            $this->request->data = $this->Content->read();
         }
     }
 
     public function view($id = null) {
-        $this->Page->id = $id;
+        $this->Content->id = $id;
 
-        if (!$this->Page->exists()) {
-            throw new NotFoundException('Invalid Page');
+        if (!$this->Content->exists()) {
+            throw new NotFoundException('Invalid Content');
         }
 
         if (!$id) {
-            $this->Session->setFlash('Invalid Page');
+            $this->Session->setFlash('Invalid Content');
             $this->redirect(array('action' => 'index'));
         }
-        $this->set('page', $this->Page->read());
+        $this->set('content', $this->Content->read());
     }
 
     public function show($id = null) {
-        $this->Page->id = $id;
+        $this->Content->id = $id;
 
-        if (!$this->Page->exists()) {
-            throw new NotFoundException('Invalid Page');
+        if (!$this->Content->exists()) {
+            throw new NotFoundException('Invalid Content');
         }
 
         if (!$id) {
-            $this->Session->setFlash('Invalid Page');
+            $this->Session->setFlash('Invalid Content');
             $this->redirect(array('action' => 'index'));
         }
-        $this->set('page', $this->Page->read());
+        $this->set('content', $this->Content->read());
         $this->layout = "default";
     }
 	

d3f16baf487567a4e20d02553b40cb46d0e74e76
BackEndCoder <steve.coverdale@gmail.com>
2014-02-22 08:31:44 +0000

    Fixed Console

--- Console/cake.php
+++ Console/cake.php
@@ -21,12 +21,11 @@ $ds = DIRECTORY_SEPARATOR;
 $dispatcher = 'Cake' . $ds . 'Console' . $ds . 'ShellDispatcher.php';
 
 if (function_exists('ini_set')) {
-	//$root = dirname(dirname(dirname(__FILE__)));
-	$root = dirname(dirname(__FILE__)) . $ds . 'Core';
+	$root = dirname(dirname(dirname(__FILE__)));
 
 	// the following line differs from its sibling
 	// /lib/Cake/Console/Templates/skel/Console/cake.php
-	ini_set('include_path', $root . $ds . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
+	ini_set('include_path', $root . $ds . 'app' . $ds . 'Core' . $ds . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
 }
 
 if (!include $dispatcher) {

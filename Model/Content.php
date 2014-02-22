7e0bcf493d3b17d8b0b4f7426450eefe04c17883
BackEndCoder <steve.coverdale@gmail.com>
2014-02-22 09:28:44 +0000

    Changes needed for ContentManager submodule

--- Model/Content.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Content extends AppModel {
-	public $useTable = 'pages';
-    public function getContent($id) {
-        return $this->find('first', array('conditions' => array('id' => $id)));
-    }
-}

e4afab2d4f56cf3c751dac9e3267a9a02d00d9be
BackEndCoder <steve.coverdale@gmail.com>
2014-03-02 17:26:44 +0000

    added Giftcards Model

--- Model/Giftcard.php
+++ Model/Giftcard.php
@@ -1,5 +1,29 @@
 <?php
+class Product extends AppModel {
 
-class Giftcard extends AppModel {
+    public $belongsTo = array('User');
+					
+ 	public $validate = array(
+ 		'title' => array(
+ 			'rule' => 'notEmpty',
+ 			'message' => 'Title cannot be left blank.'
+ 			),
+ 		'description' => array(
+ 			'rule' => 'notEmpty',
+ 			'message' => 'Description cannot be left blank.'
+ 			),
+ 		'price' => array(
+ 			'rule' => 'notEmpty',
+ 			'message' => 'Price cannot be left blank.'
+ 			),
+ 		);
 
+ 	function getGiftCards() {
+        return $this->find('all',
+            array(
+            	'limit' => 8,
+            	'order' => array('Giftcard.id DESC')
+            	)
+            );
+    }
 }
